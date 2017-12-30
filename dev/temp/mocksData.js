51a1fa8c4a1fd3da40b8697fa03413b6c2354d02
Veremey <veremey4uk@gmail.com>
2017-12-30 18:04:47 +0200

    start

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,29 @@
+'head': {
+    defaults: {
+        title: 'фан-тест',
+        useSocialMetaTags: false
+    }
+},
+
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+},
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
---
+++ markup/components/head/data/data.js
@@ -0,0 +1,6 @@
+head: {
+    defaults: {
+        title: 'фан-тест',
+        useSocialMetaTags: false
+    }
+}
